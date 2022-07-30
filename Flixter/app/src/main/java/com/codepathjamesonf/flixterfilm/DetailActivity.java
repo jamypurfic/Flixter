670b15d9799d4bddbdab9a3b7a98e8df1dd3ba76
Jameson <francisquejamy@gmail.com>
2022-07-30 12:19:19 -0400

    added youtube player api and initialized it with default vid.

--- Flixter/app/src/main/java/com/codepathjamesonf/flixterfilm/DetailActivity.java
+++ Flixter/app/src/main/java/com/codepathjamesonf/flixterfilm/DetailActivity.java
@@ -4,21 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.codepathjamesonf.flixterfilm.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_API_KEY = "AIzaSyB0Mz5RECfIfH0Dhpw8zyRzV396_AAHQ9U";
 
     TextView tvTitle;
     TextView tvOverview;
     RatingBar ratingBar;
     TextView popularity;
     TextView release_date;
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class DetailActivity extends AppCompatActivity {
         ratingBar = findViewById(R.id.ratingBar);
         popularity = findViewById(R.id.popularity);
         release_date = findViewById(R.id.release_date);
+        youTubePlayerView = findViewById(R.id.player);
 
         // ActivityTwo.java (subactivity) can access any extras passed in
             Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
@@ -38,7 +47,21 @@ public class DetailActivity extends AppCompatActivity {
             popularity.setText("Popularity: " + movie.getPopularity());
             release_date.setText("Release date: " + movie.getRelease_date());
 
+            youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    Log.d("DetailActivity", "onSuccess");
+                //anything we want to do with the video goes here
+                    youTubePlayer.cueVideo("5xVh-7ywKpE");
 
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                    Log.d("DetailActivity", "onFailure");
+
+                }
+            });
 
         }
      }
